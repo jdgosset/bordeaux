5c58b22a41be0467c8ab5cc3912e77810a384d29
gosset <dadgosset@orange.f>
2018-09-05 14:48:07 +0200

    return la nav de bordeaux

--- header.php
+++ header.php
@@ -25,37 +25,34 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bordeaux' ); ?></a>
 
 	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$bordeaux_description = get_bloginfo( 'description', 'display' );
+			if ( $bordeaux_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $bordeaux_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
 
-		<H1>BORDEAUX</H1>
-		
-		<div class="plan">
-			<ul>
-				<li><a href=""></a>quartiers</li>
-				<li><a href=""></a>plans</li>
-			</ul>
-		</div>
-
-		<div class="recherche">
-			<ul>
-				<li><a href=""></a>Aller au contenu</li>
-				<li><a href=""></a>Accessibilité</li>
-				<li><a href=""></a>Plan du site</li>
-			</ul>
-
-			<input type="text" name="" value="">
-		</div>
-
-
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bordeaux' ); ?></button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
-	<nav>
-		<img src="" alt="">
-		<ul>
-			<li>bordeaux et vous</li>
-			<li>pratique</li>
-			<li>decouvrir et sortir</li>
-			<li>entreprendre</li>
-			<li>bordeaux politique</li>
-		</ul>
-	</nav>
 	<div id="content" class="site-content">
